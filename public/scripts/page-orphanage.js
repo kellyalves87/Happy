b9abb14b88a9e96ca2cba1839572a913514d02b0
Kelly <kellyp.alves87@gmail.com>
2020-10-17 12:53:34 -0300

    page orphanage done

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,42 @@
+const options = {
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
+
+//create map
+const map = L.map("mapid", options).setView([-23.5859464, -46.7039892], 15);
+
+// create and add tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+// create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+// create and add marker
+L.marker([-23.5859464, -46.7039892], { icon }).addTo(map);
+
+//image galery
+function selectImage(event) {
+  const button = event.currentTarget;
+  button.classList.add("active");
+
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
+
+  imageContainer.src = image.src;
+
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach(removeActiveClass);
+
+  function removeActiveClass(button) {
+    button.classList.remove("active");
+  }
+}
--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,16 +1,15 @@
 //create map
-const map = L.map("mapid").setView([-23.5742517, -46.7081681], 15);
+const map = L.map("mapid").setView([-23.5859464,-46.7039892], 15);
 
-// create and add titleLayer
+// create and add tileLayer
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 // create icon
-
 const icon = L.icon({
   iconUrl: "./public/images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
-  popupAnchor: [170, 2],
+  popupAnchor: [170, 2]
 });
 
 // create popup overlay
@@ -20,11 +19,11 @@ const popup = L.popup({
   minWidth: 240,
   minHeight: 240,
 }).setContent(
-  'Lar das crianças carentes <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" > </a>'
+  'Associação Maria Helen Drexel <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" > </a>'
 );
 
 // create and add marker
-L.marker([-23.5742517, -46.7081681], { icon })
+L.marker([-23.5859464,-46.7039892], { icon })
   .addTo(map)
   .bindPopup(popup)
 
