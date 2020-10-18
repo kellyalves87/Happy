2869e391789edb14250b508bffeeeef9feae1f3a
Kelly <kellyp.alves87@gmail.com>
2020-10-18 14:48:10 -0300

    initializing backend

---
+++ src/database/fakedata.js
@@ -0,0 +1,36 @@
+module.exports = [
+  {
+    id: 1,
+    lat: "-23.5859464",
+    lng: "-46.7039892",
+    name: "Associação Maria Helen Drexel",
+    description:
+      "Organização civil que acolhe crianças e adolescentes, cujos direitos básicos tenham sido violados ou ameaçados, proporcionando proteção, assistência, saúde e educação visando o seu desenvolvimento.",
+    images: [
+      "https://images.unsplash.com/photo-1567974062356-dc24af997614?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1601725591042-d657de65a47b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    ],
+    instructions:
+      "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horários de visitas Das 08h até as 18h",
+    open_on_weekends: "1",
+  },
+  {
+    id: 2,
+    lat: "-23.5859464",
+    lng: "-46.7139892",
+    name: "Associação Ana Maria Braga",
+    description:
+      "Organização civil que acolhe crianças e adolescentes, cujos direitos básicos tenham sido violados ou ameaçados, proporcionando proteção, assistência, saúde e educação visando o seu desenvolvimento.",
+    images: [
+      "https://images.unsplash.com/photo-1567974062356-dc24af997614?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1601725591042-d657de65a47b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    ],
+    instructions:
+      "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horários de visitas Das 08h até as 18h",
+    open_on_weekends: "0",
+  },
+];
